c976ebd5fa38f092451755cf21e00a87b52db46a
changjun <52chinaweb@gmail.com>
2023-05-16 00:49:45 +0800

    chore(bumpp): files change

---
+++ bumpp.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from 'bumpp'
+
+export default defineConfig({
+  // https://github.com/antfu/bumpp/blob/main/src/types/version-bump-options.ts
+  files: ['package.json', 'package-lock.json', 'README.md'],
+})
