864ccd441d40d7489eda192a71ffce6414da726b
changjun <52chinaweb@gmail.com>
2023-05-16 01:14:46 +0800

    chore: bump file change

--- bump.config.ts
+++ bump.config.ts
@@ -3,7 +3,4 @@ import { defineConfig } from 'bumpp'
 export default defineConfig({
   // https://github.com/antfu/bumpp/blob/main/src/types/version-bump-options.ts
   files: ['package.json', 'package-lock.json', 'README.md'],
-  push: false,
-  commit: false,
-  tag: false,
 })
